2b7eef6485a47332be178498079b73adbcf2bb98
unknown <hnfhamany@gmail.com>
2025-06-19 14:06:09 +0700

    Custom POS's Recall Button

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Hide Recall Button in POS Preparation Display',
+    'version': '1.0',
+    'category': 'Point of Sale',
+    'depends': ['point_of_sale', 'pos_preparation_display'],
+    'assets': {
+        'point_of_sale.assets': [
+            'custom_pos_hide_recall_button/static/src/css/hide_recall_button.css',
+        ],
+    },
+    'installable': True,
+    'application': False,
+}
